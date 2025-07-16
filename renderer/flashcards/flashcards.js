81e55f537d94f56a65e1c27296232b11cfe41b40
Blorg132 <blorgsnat@gmail.com>
2025-07-16 11:32:20 -0700

    bacjk buton

--- renderer/flashcards/flashcards.js
+++ renderer/flashcards/flashcards.js
@@ -9,6 +9,9 @@ let flashcards = JSON.parse(fs.readFileSync(flashcardPath));
 let currentIndex = 0;
 let isFlipped = false;
 
+console.log(flashcards);                 // should print an array of objects
+console.log(flashcards[0]);              // should print first flashcard object
+console.log(flashcards[0].front);        // should print front text of first card
 
 // Flipping/Card functions
 function showFront() {
