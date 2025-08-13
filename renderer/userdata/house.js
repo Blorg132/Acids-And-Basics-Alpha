9652abe67eaa8369fc2f3f0447a655b27f9b330e
Blorg132 <blorgsnat@gmail.com>
2025-08-13 09:49:00 -0700

    incorporating house.json file pt. 1

--- renderer/userdata/house.js
+++ renderer/userdata/house.js
@@ -12,55 +12,76 @@ let thereclinerplaced = false;
 let pooltableowned = false;
 let pooltableplaced = false;
 
+//Getting Clickables from The Document///////////////////
+popupwindow = document.getElementById("selectionwindow");
 
-// Checking Previous House Saves
+woodenchair = document.getElementById("woodenchair");
+metalchair = document.getElementById("metalchair");
+therecliner = document.getElementById("therecliner");
 
+pooltable = document.getElementById("pooltable");
+//////////////////////////////////////////////////////////
+
+// Loading in house saves from disk!!!!//////////////////////////////////////
+let rawHouseData = fs.readFileSync("./renderer/userdata/house.json", "utf8");
+let houseData = JSON.parse(rawHouseData);
+console.log(houseData);
+/////////////////////////////////////////////////////////////////////////////
+
+//Checking to see what furniture was previously placed///////////////////////
+if(houseData.chair === "woodenchair"){
+    woodenchairplaced = true;
+    document.getElementById("physicalwoodenchair").classList.remove("hidden");
+}else if(houseData.chair === "metalchair"){
+    metalchairowned = true;
+    document.getElementById("physicalmetalchair").classList.remove("hidden");
+}else if(houseData.chair === "therecliner"){
+    thereclinerplaced = true;
+    document.getElementById("physicaltherecliner").classList.remove("hidden");
+}
+console.log("Chair loaded!");
+
+if(houseData.table === "pooltable"){
+    pooltableplaced = true;
+    document.getElementById("physicalpooltable").classList.remove("hidden");
+}
+/////////////////////////////////////////////////////////////////////////////
 
 function furnishClicked(){
-    popupwindow = document.getElementById("selectionwindow");
     popupwindow.classList.remove("hidden");
 
-    //Getting Clickables from Each Furniture
-
-    woodenchair = document.getElementById("woodenchair");
-    metalchair = document.getElementById("metalchair");
-    therecliner = document.getElementById("therecliner");
-
-    pooltable = document.getElementById("pooltable");
-
-
-
+    //I made all the clickables global.... thank me later.... just kidding it kind of does jackshit, 
+    //I just hated how it looked over here
     //
     // Reading Inventory from Disk
     //
 
 
     // NOTE: Readfilesync breaks this. i have no idea why, but i'm assuming it'll fuck something up in the future.
-    fs.readFile("./renderer/userdata/inventory.json", "utf8", (err, jsonString) => {
-        inventoryData = JSON.parse(jsonString);
+    let inventoryRaw = fs.readFileSync("./renderer/userdata/inventory.json", "utf8");
+    let inventoryData = JSON.parse(inventoryRaw);
 
-        // Checking to see what inventory pieces are owned
-        //CHAIRS
-        if(inventoryData.items.some(item => item.id === 101)){
-            woodenchairowned = true;
-            document.getElementById("woodenchair").classList.remove("unavailable");
-        }
-        if(inventoryData.items.some(item => item.id === 102)){
-            metalchairowned = true;
-            document.getElementById("metalchair").classList.remove("unavailable");
-        }
-        if(inventoryData.items.some(item => item.id === 103)){
-            thereclinerowned = true;
-            document.getElementById("therecliner").classList.remove("unavailable");
-        }
+    // Checking to see what inventory pieces are owned
+    //CHAIRS
+    if(inventoryData.items.some(item => item.id === 101)){
+        woodenchairowned = true;
+        document.getElementById("woodenchair").classList.remove("unavailable");
+    }
+    if(inventoryData.items.some(item => item.id === 102)){
+        metalchairowned = true;
+        document.getElementById("metalchair").classList.remove("unavailable");
+    }
+    if(inventoryData.items.some(item => item.id === 103)){
+        thereclinerowned = true;
+        document.getElementById("therecliner").classList.remove("unavailable");
+    }
 
-        //TABLES
+    //TABLES
 
-        if(inventoryData.items.some(item => item.id === 104)){
-            pooltableowned = true;
-            document.getElementById("pooltable").classList.remove("unavailable");
-        }
-    });
+    if(inventoryData.items.some(item => item.id === 104)){
+        pooltableowned = true;
+        document.getElementById("pooltable").classList.remove("unavailable");
+    }
 
     //
     //
