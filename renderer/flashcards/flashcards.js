da9190818cf761693c0a3cbd7c7d2903bd41d028
Blorg132 <blorgsnat@gmail.com>
2025-08-14 16:36:13 -0700

    OMG FLASHCARDS IS BACK

--- renderer/flashcards/flashcards.js
+++ renderer/flashcards/flashcards.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-
 const flashcards = [
 
   // If you're ever interested into what each of the units mean, here is what i have so far:
@@ -87,18 +85,114 @@ const flashcards = [
 
 
 let currentIndex = 0;
-
-@@ -6,7 +94,6 @@ let isFlipped = false;
+let isFlipped = false;
 let monkey = 0;
 let cardText = document.getElementById('card-text')
 let usedFlashcards = []
-let unitSelected = 1;
 
 //counter stuff yet again
 let flashcardcounter = document.getElementById("flashcardcounter");
+flashcardcounter.textContent = usedFlashcards.length;
+
+let dog = document.getElementById("flashcardsleft");
+dog.textContent = flashcards.length - usedFlashcards.length;
+
+let buttonsound = document.getElementById("buttonclicked");
+buttonsound.volume = 0.15;
+
+function showFront() {
+  cardText.textContent = flashcards[currentIndex].front;
+  cardText.classList.add('frontside');
+}
+
+function flipCard() {
+  buttonsound.currentTime = 0;
+  buttonsound.play();
+  isFlipped = !isFlipped;
+  cardText.textContent = isFlipped
+    ? flashcards[currentIndex].back
+    : flashcards[currentIndex].front;
+
+  if (isFlipped) {
+    cardText.classList.add('backside');
+    cardText.classList.remove('frontside');
+  } else {
+    cardText.classList.remove('backside');
+    cardText.classList.add('frontside');
+  }
+}
+
+function nextCard() {
+  buttonsound.currentTime = 0;
+  buttonsound.play();
+  if(cardText.classList.contains('backside')){
+    cardText.classList.remove('backside')
+  }
+  var numberFound = false;
+  var previousIndex = currentIndex;
+
+  // this is for the counters
+  flashcardcounter.textContent = usedFlashcards.length;
+  dog.textContent = flashcards.length;
 
 
-@@ -114,67 +201,4 @@ function shuffleFlashcards(){
+  // check if all flashcards have been used
+  if(usedFlashcards.length === flashcards.length){
+    flashcardcounter.classList.remove('settle');
+    void flashcardcounter.offsetWidth; // Force reflow (resets the animation)
+    usedFlashcards = [];
+    emptysound = document.getElementById("emptysound");
+    emptysound.play();
+    flashcardcounter.classList.add('completed-set');
+    flashcardcounter.classList.add('settle');
+  } else {
+    flashcardcounter.classList.remove('completed-set');
+  }
+
+  // entering decision loop
+  while (numberFound == false){
+    currentIndex = Math.floor(Math.random() * flashcards.length);
+    if (currentIndex == previousIndex){ // checks if card is same as previous one
+      console.log('ruhroh');
+    } else {
+      if (usedFlashcards.includes(currentIndex)){ // checks if card has already been used
+        ('double ruhroh');
+      } else {
+        //appending Used Flashcard if both tests pass :)
+        usedFlashcards.push(currentIndex);
+        numberFound = true;
+      }
+    }
+  }
+  
+  // (currentIndex + random.number????) make a shuffle mechanic this way? built in?
+  // then again, flashcards should be based on what units you want to test, right?
+  isFlipped = false;
+  showFront();
+}
+
+
+
+function shuffleFlashcards(){
+  cardText.classList.remove('settle');
+  void cardText.offsetWidth; // Force reflow (resets the animation)
+  const shuffleSounds = [
+    new Audio('../sounds/shufflesound1.wav'),
+    new Audio('../sounds/shufflesound2.wav')
+  ];
+
+  // Function to play a random shuffle sound
+  const randomIndex = Math.floor(Math.random() * shuffleSounds.length);
+  shuffle = shuffleSounds[randomIndex]
+  shuffle.volume = 0.2;
+  shuffle.play();
+  cardText.classList.add('settle');
+
+  usedFlashcards = [];
+  nextCard();
+}
+
+
 // make a unit select button
 
 
